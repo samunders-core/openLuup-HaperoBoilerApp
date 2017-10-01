307b11249869d49a9855c7f5b926fe543153a1c5
Samunders Core <sam_@centrum.sk>
2017-10-01 12:02:58 +0200

    working communication

--- L_HaperoBoiler.lua
+++ L_HaperoBoiler.lua
@@ -7,19 +7,108 @@ ABOUT = {
   DOCUMENTATION = "https://github.com/samunders-core/...",
 }
 
--- plugin initialisation
-function hbStartup(d, relayId, controllerId)
-  devNo = d  
-  _log "starting..." 
-  display (ABOUT.NAME,'')  
-  
-  do -- version number
-    local y,m,d = ABOUT.VERSION:match "(%d+)%D+(%d+)%D+(%d+)"
-    local version = ("v%d.%d.%d"): format (y%2000,m,d)
-    setVar ("Version", version)
-    _log (version)
+local devNo                      -- our device number
+local ip                         -- OBD2 WiFi dongle ip:port
+local timeoutSeconds = 5
+
+local function readNonEmptyLines()
+  local function iterator(state, i)
+    luup.io.intercept(devNo)
+    local response = luup.io.read(timeoutSeconds, devNo)
+    if response and "" ~= response then
+      return response
+    end
+    return
   end
-  
-  set_failure (0)
-  return true, "OK", ABOUT.NAME
+  return iterator, nil, nil
 end
+
+local function log(message)
+  if message then
+    luup.log(ABOUT.NAME .. " boiler at " .. ip .. " - " .. message)
+  end
+end
+
+local function status(message)
+  local result = not message or "OK" == message
+  luup.set_failure(result and 0 or 1)
+  return result, message or "OK", ABOUT.NAME
+end
+
+local function command(cmd, ...)
+  log(cmd)
+  luup.io.intercept(devNo)
+  if not luup.io.write(cmd, devNo) then
+    log("connect call failed")
+    return status("CAN-Bus gateway communication failed")
+  end      -- should respond with unquoted "cmd\rOK\r\r>"
+  for _, expected in ipairs({...}) do
+    luup.io.intercept(devNo)
+    local response = luup.io.read(timeoutSeconds, devNo)
+    if expected == response or ('>' .. expected) == response then
+      log(response)
+    else
+      log(cmd .. " failed - received '" .. (response or "nil") .. "' instead of '" .. expected .. "'")
+      return status("CAN-Bus gateway communication failed")
+    end
+  end
+  local message
+  for response in readNonEmptyLines() do
+    log(cmd .. " - received '" .. response .. "'")
+    message = response
+  end
+  return true, message, ABOUT.NAME
+end
+
+local function atCommand(cmd)
+  cmd = "AT " .. cmd
+  local result, message, name = command(cmd, cmd, "OK")
+  return result, message, name
+end
+
+function received(data)
+  log(data)
+end
+
+-- plugin initialisation/reconnect
+function pollHapero(d)
+  if not devNo then
+    devNo = d
+  end
+
+  ip = luup.attr_get("ip", devNo)
+  if not ip then
+    luup.log(ABOUT.NAME .. " manager: missing ip=address:port property")
+    return status("CAN-Bus gateway address not set")
+  end
+  local ipAddress = string.match(ip, '^(%d%d?%d?%.%d%d?%d?%.%d%d?%d?%.%d%d?%d?)')
+  local ipPort    = string.match(ip, ':(%d+)$')
+
+  log("polling...")
+  if d or not luup.io.is_connected(devNo) then
+    luup.io.open(devNo, ipAddress, ipPort)
+    local cmds = {"L0", "H1", "S1", "SH 07C"}
+    for _, cmd in ipairs(cmds) do
+      local result, message, name = atCommand(cmd)
+      if not result then
+        luup.call_delay("pollHapero", 30, luup.devices[devNo].local_udn or tostring(d))
+        return status(message)
+      end
+    end
+  end
+
+  local cmd = "DE 00 00"
+  local valid, data, name = command(cmd)	-- expecting 07B 8 DE 00 00 00 00 00 FF 00
+  if valid then
+    luup.call_delay("pollHapero", 30, nil)    -- state changes 1/min tops
+    local expected = "07B 8 DE "
+    if string.sub(data, 1, string.len(expected)) == expected then
+      log(data)
+    else
+      log("protocol mismatch: '" .. data .. "' received as response for '" .. cmd .. "'")
+    end
+  end
+
+  return status("OK")
+end
+
